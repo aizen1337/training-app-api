34b4e81e9897685a8d443a6ab608e19b6e2ea375
AizeN1337 <maciejkalata79@gmail.com>
2024-04-04 22:27:14 +0200

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
+import { logger } from "@bogeychan/elysia-logger";
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { autoroutes } from "elysia-autoroutes";
+import { clerkPlugin } from "elysia-clerk";
+import { rateLimit } from "elysia-rate-limit";
+import { requestID } from "elysia-requestid";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.use(logger({
+  timestamp: true,
+  autoLogging: true,
+  crlf: true,
+  customProps(ctx) {
+    return ctx
+  },
+}))
+.use(swagger())
+.use(rateLimit())
+.use(clerkPlugin())
+.use(requestID())
+.use(autoroutes(
+  {
+    prefix: '/api'
+  }
+))
+.listen(3000);
+
+export type ElysiaApp = typeof app
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
